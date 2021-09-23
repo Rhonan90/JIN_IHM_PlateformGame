13660972104fd054211e51b341b840d86e0a3ae2
Quentin MOREELS <quentin.moreels@hotmail.fr>
2021-09-23 13:06:33 +0200

    Ajout du Raycast pour la detection du sol

--- JIN_IHM_PlateformGame/Assets/Scripts/InputController.cs
+++ JIN_IHM_PlateformGame/Assets/Scripts/InputController.cs
@@ -2,60 +2,55 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum playerState
-{
-    Grounded,
-    Flying
-};
-
 public class InputController : MonoBehaviour
 {
+    [SerializeField] private LayerMask floorLayerMask;
+    private BoxCollider2D boxCollider2d;
+
     private Vector2 position;
     private Vector2 speed;
     private Vector2 speedInput;
 
     private bool jump = false;
-    private float airTime = 10;
+    private float timeSinceJump = 0;
 
-    public float movementSpeed = 20;
-    public float inertia = 0.95f;
-    public float jumpForce = 10 ;
-    public float customGravity = -10;
-
-    playerState state = playerState.Flying;
+    public float movementSpeed = 20;        //Vitesse du joueur
+    public float inertia = 0.5f;            //Inertie du joueur
+    public float jumpForce = 10 ;           //Vitesse du saut
+    public float customGravity = -10;       //Force de gravite
+    public float airTime = 0.2f;            //Temps en maximum dans les airs lors du saut
 
     private void Awake()
     {
         position = transform.position;
-
-        this.gameObject.SetActive(false);
-        
+        boxCollider2d = transform.GetComponent<BoxCollider2D>();
     }
 
     private void Update()
     {
         getInput();
 
-        speed = speedInput * Time.deltaTime * movementSpeed + inertia * speed; ;  //simule l'accélération et une certaine inertie     
+        speed = speedInput * movementSpeed + inertia * speed;  //simule l'accélération et une certaine inertie     
 
-        airTime += Time.deltaTime;
-        if (jump && (state == playerState.Grounded  || airTime < 0.3) )
-        {
-            speed.y = jumpForce * Time.deltaTime;
-        }
+        if (IsGrounded()) //Incremente temps passé depuis l'appui de la touche de saut (0 si au sol)
+            timeSinceJump = 0;
         else
-        {
-            speed.y = customGravity * Time.deltaTime;
-        }
+            timeSinceJump += Time.deltaTime;
+        
+        if (jump && (IsGrounded()  || timeSinceJump < airTime) ) //Gestion vitesse verticale
+            speed.y = jumpForce;
+        else
+            speed.y = customGravity;
 
-        if (state == playerState.Grounded && speed.y < 0)  //anti clipping dans le sol
+        if (IsGrounded() && speed.y < 0)  //anti clipping dans le sol
             speed.y = 0;
+
+        if (IsTouchingWall())  //anti clipping dans le sol
+            speed.x = 0;
 
         position.x += speed.x * Time.deltaTime;  //horitontal movement
         position.y += speed.y * Time.deltaTime;  //vertical movement
         transform.position = position;  //update position
-
-        Debug.Log(state);
     }
 
     private void getInput()
@@ -64,19 +59,50 @@ public class InputController : MonoBehaviour
         jump = Input.GetButton("Jump");
     }
 
-    private void OnCollisionStay2D(Collision2D collision)
+    private bool IsGrounded()   //Utilisation du raycast pour detecter les collisions au sol
     {
-        if (collision.gameObject.CompareTag("Ground"))
+        RaycastHit2D raycastHit = Physics2D.Raycast(boxCollider2d.bounds.center, Vector2.down, boxCollider2d.bounds.extents.y, floorLayerMask);
+
+        Color rayColor; //Debut debuging
+        if (raycastHit.collider != null)
         {
-            state = playerState.Grounded;
-            airTime = 0;
+            rayColor = Color.green;
         }
+        else
+        {
+            rayColor = Color.red;
+        }
+        Debug.DrawRay(boxCollider2d.bounds.center, Vector2.down * (boxCollider2d.bounds.extents.y), rayColor);
+        Debug.Log(raycastHit.collider); //Fin debuging
+
+        return raycastHit.collider != null;
     }
 
-    private void OnCollisionExit2D(Collision2D collision)
+    private bool IsTouchingWall()   //Utilisation du raycast pour detecter les collisions au sol
     {
-        if (collision.gameObject.CompareTag("Ground"))
-            state = playerState.Flying;
-    }
+        float extraHeightText = .1f;
+        RaycastHit2D raycastHitLeft = Physics2D.Raycast(boxCollider2d.bounds.center, Vector2.left, boxCollider2d.bounds.extents.x + extraHeightText, floorLayerMask);
+        RaycastHit2D raycastHitRight = Physics2D.Raycast(boxCollider2d.bounds.center, Vector2.right, boxCollider2d.bounds.extents.x + extraHeightText, floorLayerMask);
 
+        Color rayColorLeft; //Debut debuging
+        Color rayColorRight;
+
+        if (raycastHitLeft.collider != null)
+            rayColorLeft = Color.green;
+        else
+            rayColorLeft = Color.red;
+
+        if (raycastHitRight.collider != null)
+            rayColorRight = Color.green;
+        else
+            rayColorRight = Color.red;
+
+        Debug.DrawRay(boxCollider2d.bounds.center, Vector2.left * (boxCollider2d.bounds.extents.x + extraHeightText), rayColorLeft);
+        Debug.DrawRay(boxCollider2d.bounds.center, Vector2.right * (boxCollider2d.bounds.extents.x + extraHeightText), rayColorRight);
+
+        Debug.Log(raycastHitLeft.collider);
+        Debug.Log(raycastHitRight.collider);//Fin debuging
+
+        return raycastHitLeft.collider != null && raycastHitRight.collider != null;
+    }
 }
