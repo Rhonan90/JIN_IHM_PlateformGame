4e153adc38065a069739f0642af0c5a2b11bae77
Rhonan90 <paul.cathrine@orange.fr>
2021-10-03 14:29:36 +0200

    Changement de la gestion des collisions
    encore des petits problèmes de collision sur les coins

--- JIN_IHM_PlateformGame/Assets/Scripts/InputController1.cs
+++ JIN_IHM_PlateformGame/Assets/Scripts/InputController1.cs
@@ -6,6 +6,7 @@ public class InputController1 : MonoBehaviour
 {
     [SerializeField] private LayerMask floorLayerMask;
     private BoxCollider2D boxCollider2d;
+    private Collider2D collider2d;
 
     private Vector2 position;
     private Vector2 speed;
@@ -28,19 +29,14 @@ public class InputController1 : MonoBehaviour
     public float sprintVelocityModifier = 1.2f;   //Multiplicateur de vitesse en sprintant
     public float sprintJumpModifier = 1.2f;       //Multiplicateur de force de saut en srprintant
 
-
-    private bool touchingRight;             //Test si mur à droite pour les collisions
-    private bool touchingLeft;              //Test si mur à gauche pour les collisions
-    private bool touchingTop;               //Test si mur au dessus pour les collisions
     private bool touchingDown;              //Test si mur en dessous pour les collisions
-
-    private float extraRaycastCheck = 0.1f;
 
 
     private void Awake()
     {
         position = transform.position;
         boxCollider2d = transform.GetComponent<BoxCollider2D>();
+        collider2d = transform.GetComponent<Collider2D>();
         speed = new Vector2();
         speedIncrement = new Vector3();
     }
@@ -52,14 +48,16 @@ public class InputController1 : MonoBehaviour
         acceleration = (inputs + new Vector2(0, customGravity)) / mass;
         speed.x =  acceleration.x * Time.deltaTime * movementSpeed + inertiaFactor * speed.x; // simule une certaine inertie  
         speed.y = acceleration.y * Time.deltaTime; 
-        speed.x = Mathf.Clamp(speed.x, -maxMovementSpeed, maxMovementSpeed);
+        speed.x = Mathf.Clamp(speed.x, -maxMovementSpeed, maxMovementSpeed);  // on limite la vitesse maximale du joueur
 
         if (sprint)
         {
             speed.x *= sprintVelocityModifier;  //TODO : fix
         }
 
-        if (IsGrounded()) //Incremente temps passé depuis l'appui de la touche de saut (0 si au sol)
+        touchingDown = (CheckCollisions(collider2d, new Vector2(0, speed.y).normalized, Mathf.Abs(speed.y) * Time.deltaTime)) ; //test si on touche le sol
+        
+        if (touchingDown) //Incremente temps passé depuis l'appui de la touche de saut (0 si au sol)
         {
             timeSinceJump = 0;
             canDoubleJump = true; //resest du double saut quand on touche le sol
@@ -78,29 +76,13 @@ public class InputController1 : MonoBehaviour
             canDoubleJump = false;
         }
         else
-            speed.y += customGravity * Time.deltaTime;    //Le cube tombe  //TODO : à fix
-
-        IsGrounded();
-        if ((touchingDown && speed.y < 0) && !(jump))  //collision en bas
-        {
-            speed.y = 0;
-        }
-
-        IsTouchingWall();
-        if (touchingLeft && speed.x < 0 || touchingRight && speed.x > 0)  //collision à gauche
-        {
-            speed.x = 0;
-        }
-
-        IsTouchingCeiling();
-        if (touchingTop && speed.y > 0) //collision en haut
-        {
-            speed.y = 0;
-        }        
+            speed.y += customGravity * Time.deltaTime;    //Le cube tombe  
 
 
-        position.x += speed.x * Time.deltaTime;  //horitontal movement
-        position.y += speed.y * Time.deltaTime;  //vertical movement
+        if (!CheckCollisions(collider2d, new Vector2(speed.x,0).normalized, Mathf.Abs(speed.x) * Time.deltaTime))  //test pour vérifier qu'on entre pas dans un mur
+            position.x += speed.x * Time.deltaTime;  //horitontal movement
+        if (!CheckCollisions(collider2d, new Vector2(0, speed.y).normalized, Mathf.Abs(speed.y) * Time.deltaTime)) //test pour vérifier qu'on entre pas dans le sol ou le plafond
+            position.y += speed.y * Time.deltaTime;  //vertical movement
         speedIncrement = new Vector3(speed.x * Time.deltaTime, speed.y * Time.deltaTime, 0);  //sert à caster en avance pour les collisions
         transform.position = position;  //update position
     }
@@ -112,75 +94,24 @@ public class InputController1 : MonoBehaviour
         sprint = Input.GetButton("Sprint");
     }
 
-    private bool IsGrounded()   //Utilisation du raycast pour detecter les collisions au sol
+    private bool CheckCollisions(Collider2D moveCollider, Vector2 direction, float distance)
     {
-        RaycastHit2D raycastHitDown = Physics2D.Raycast(boxCollider2d.bounds.min - new Vector3(0, extraRaycastCheck, 0) + speedIncrement, Vector2.right, boxCollider2d.bounds.size.x, floorLayerMask);
-
-        Color rayColor; //Debut debuging
-        if (raycastHitDown.collider != null)
+        if (moveCollider!=null)
         {
-            rayColor = Color.green;
+            RaycastHit2D[] hits = new RaycastHit2D[10];
+            ContactFilter2D filter = new ContactFilter2D() { };
+
+            int numHits = moveCollider.Cast(direction, filter, hits, distance);
+
+            for (int i = 0; i < numHits; i++)
+            {
+                if (!hits[i].collider.isTrigger)
+                {
+                    return true;
+                }
+            }
         }
-        else
-        {
-            rayColor = Color.red;
-        }
-        Debug.DrawRay(boxCollider2d.bounds.min - new Vector3(0, extraRaycastCheck, 0), Vector2.right * boxCollider2d.bounds.size.x, rayColor);
-        Debug.Log(raycastHitDown.collider); //Fin debuging
-
-        touchingDown = raycastHitDown.collider != null ;
-
-        return touchingDown;
-    }
-
-    private bool IsTouchingWall()   //Utilisation du raycast pour detecter les collisions au sol
-    {
-        RaycastHit2D raycastHitLeft = Physics2D.Raycast(boxCollider2d.bounds.min - new Vector3(extraRaycastCheck, 0, 0) + speedIncrement, Vector2.up, boxCollider2d.bounds.size.y, floorLayerMask);
-        RaycastHit2D raycastHitRight = Physics2D.Raycast(boxCollider2d.bounds.max + new Vector3(extraRaycastCheck, 0, 0) + speedIncrement, Vector2.down, boxCollider2d.bounds.size.y, floorLayerMask);
-
-        Color rayColorLeft; //Debut debuging
-        Color rayColorRight;
-
-        if (raycastHitLeft.collider != null)
-            rayColorLeft = Color.green;
-        else
-            rayColorLeft = Color.red;
-
-        if (raycastHitRight.collider != null)
-            rayColorRight = Color.green;
-        else
-            rayColorRight = Color.red;
-
-        Debug.DrawRay(boxCollider2d.bounds.min - new Vector3(extraRaycastCheck, 0, 0), Vector2.up * (boxCollider2d.bounds.size.y), rayColorLeft);
-        Debug.DrawRay(boxCollider2d.bounds.max + new Vector3(extraRaycastCheck, 0, 0), Vector2.down * (boxCollider2d.bounds.size.y), rayColorRight);
-
-        Debug.Log(raycastHitLeft.collider);
-        Debug.Log(raycastHitRight.collider);//Fin debuging
-
-        touchingLeft = raycastHitLeft.collider != null;
-        touchingRight = raycastHitRight.collider != null;
-
-        return touchingLeft || touchingRight;
-    }
-
-    private bool IsTouchingCeiling()   //Utilisation du raycast pour detecter les collisions au sol
-    {
-        RaycastHit2D raycastHitTop = Physics2D.Raycast(boxCollider2d.bounds.max + new Vector3(0, extraRaycastCheck, 0) + speedIncrement, Vector2.left, boxCollider2d.bounds.size.x, floorLayerMask);
-
-        Color rayColor; //Debut debuging
-        if (raycastHitTop.collider != null)
-        {
-            rayColor = Color.green;
-        }
-        else
-        {
-            rayColor = Color.red;
-        }
-        Debug.DrawRay(boxCollider2d.bounds.max + new Vector3(0, extraRaycastCheck, 0), Vector2.left * boxCollider2d.bounds.size.x, rayColor);
-        Debug.Log(raycastHitTop.collider); //Fin debuging
-
-        touchingTop = raycastHitTop.collider != null;
-        return touchingTop;
+        return false;
     }
 
 }
